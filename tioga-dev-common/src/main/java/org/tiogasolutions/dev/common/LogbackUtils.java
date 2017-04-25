35943e20d87dbdeb18cdd99d00d885fd4376d54d
Jacob D Parr <me@jacobparr.com>
2017-04-24 21:30:15 -0700

    Updated LogbackUtils to make it easier to change the log level.

--- tioga-dev-common/src/main/java/org/tiogasolutions/dev/common/LogbackUtils.java
+++ tioga-dev-common/src/main/java/org/tiogasolutions/dev/common/LogbackUtils.java
@@ -6,6 +6,7 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.ConsoleAppender;
 import ch.qos.logback.core.LogbackException;
 import ch.qos.logback.core.joran.spi.JoranException;
@@ -19,76 +20,90 @@ import java.nio.file.Path;
 
 public class LogbackUtils {
 
-  private static final org.slf4j.Logger log = LoggerFactory.getLogger(LogbackUtils.class);
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(LogbackUtils.class);
 
-  public static void initLogback(Level level) {
-
-    // Reroute java.util.Logger to SLF4J
-    SLF4JBridgeHandler.removeHandlersForRootLogger();
-    SLF4JBridgeHandler.install();
-
-    ILoggerFactory factory = LoggerFactory.getILoggerFactory();
-    if (factory instanceof LoggerContext == false) {
-      throw new LogbackException("Expected LOGBACK binding with SLF4J, but another log system has taken the place: " + factory.getClass().getSimpleName());
+    public static void setRootLevel(Level level) {
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(level);
     }
 
-    LoggerContext context = (LoggerContext)factory;
-
-    PatternLayoutEncoder ple = new PatternLayoutEncoder();
-    String pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n";
-    ple.setPattern(pattern);
-    ple.setContext(context);
-    ple.start();
-
-    ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<>();
-    appender.setName("STDOUT");
-    appender.setContext(context);
-    appender.setEncoder(ple);
-
-    Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-    root.setLevel(level);
-    root.addAppender(appender);
-
-    log.info("Using default logback config: {} ({})", level, pattern);
-  }
-
-  public static Path initLogback(Path configDir, String propertyName, String fileName) {
-
-    // Configure everything to initially log to the console.
-    initLogback(Level.WARN);
-
-    if (configDir == null) {
-      log.info("Config directory not specified, using default logback config.");
-      return null;
-    } else if (Files.exists(configDir) == false) {
-      log.info("Config directory ({}) does not exist, using default logback config.", configDir);
-      return null;
+    public static void addRootAppender(Appender<ILoggingEvent> appender) {
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.addAppender(appender);
     }
 
-    String logConfigArg = EnvUtils.findProperty(propertyName, fileName);
-    Path logConfigFile = configDir.resolve(logConfigArg);
+    public static void initLogback(Level level) {
 
-    if (Files.notExists(logConfigFile)) {
-      log.info("Missing {}, using default logback config.", logConfigFile.toString());
-      return null;
+        // Reroute java.util.Logger to SLF4J
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+
+        ILoggerFactory factory = LoggerFactory.getILoggerFactory();
+        if (factory instanceof LoggerContext == false) {
+            throw new LogbackException("Expected LOGBACK binding with SLF4J, but another log system has taken the place: " + factory.getClass().getSimpleName());
+        }
+
+        LoggerContext context = (LoggerContext) factory;
+
+        PatternLayoutEncoder ple = new PatternLayoutEncoder();
+        String pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n";
+        ple.setPattern(pattern);
+        ple.setContext(context);
+        ple.start();
+
+        ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<>();
+        appender.setName("STDOUT");
+        appender.setContext(context);
+        appender.setEncoder(ple);
+
+        setRootLevel(level);
+        addRootAppender(appender);
+
+        log.info("Using default logback config: {} ({})", level, pattern);
     }
 
-    log.info("Configured logging from  {}", logConfigFile.toString());
-    LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+    @Deprecated
+    public static Path initLogback(Path configDir, String propertyName, String fileName) {
+        return initLogback(Level.WARN, configDir, propertyName, fileName);
+    }
 
-    try {
-      JoranConfigurator configurator = new JoranConfigurator();
-      configurator.setContext(context);
+    public static Path initLogback(Level level, Path configDir, String propertyName, String fileName) {
 
-      // Call context.reset() to clear any previous configuration, e.g. default
-      // configuration. For multi-step configuration, omit calling context.reset().
-      context.reset();
-      configurator.doConfigure(logConfigFile.toString());
+        // Configure everything to initially log to the console.
+        initLogback(level);
 
-    } catch (JoranException ignored) {/* ignored */}
+        if (configDir == null) {
+            log.info("Config directory not specified, using default logback config.");
+            return null;
+        } else if (Files.exists(configDir) == false) {
+            log.info("Config directory ({}) does not exist, using default logback config.", configDir);
+            return null;
+        }
 
-    StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+        String logConfigArg = EnvUtils.findProperty(propertyName, fileName);
+        Path logConfigFile = configDir.resolve(logConfigArg);
 
-    return logConfigFile;
-  }
+        if (Files.notExists(logConfigFile)) {
+            log.info("Missing {}, using default logback config.", logConfigFile.toString());
+            return null;
+        }
+
+        log.info("Configured logging from  {}", logConfigFile.toString());
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        try {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(context);
+
+            // Call context.reset() to clear any previous configuration, e.g. default
+            // configuration. For multi-step configuration, omit calling context.reset().
+            context.reset();
+            configurator.doConfigure(logConfigFile.toString());
+
+        } catch (JoranException ignored) {/* ignored */}
+
+        StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+
+        return logConfigFile;
+    }
 }
